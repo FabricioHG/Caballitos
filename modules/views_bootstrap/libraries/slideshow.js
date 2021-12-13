173f0c0ced97fd8a28c60bc30b33b2850d58567c
Rama Fabricio <fabriciofranciscohernandezgome@gmail.com>
2021-12-13 15:59:09 -0600

    iniciando a trabajar con git

---
+++ modules/views_bootstrap/libraries/slideshow.js
@@ -0,0 +1,12 @@
+(function ($) {
+
+  $('#playButtonviews-bootstrap-slideshow-principal-block-1').click(function () {
+    $('#views-bootstrap-slideshow-principal-block-1').carousel('cycle');
+  });
+  $('#pauseButtonviews-bootstrap-slideshow-principal-block-1').click(function () {
+    $('#views-bootstrap-slideshow-principal-block-1').carousel('pause');
+  });
+
+
+
+  }(jQuery));
---
+++ sites/default/files/php/twig/619d248c0a3e6___string_template__c74f7d_qOWfATn--PdkyrSQDe9ccID2h/fO9V5ZCubqI-gReMbKtcOwnuYLpj5LnqwI9Cb78qg7I.php
@@ -0,0 +1,89 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__c74f7d902ba7012a2a03774c8e2e47e3fbaae964cc9f93f530bb52b60626ccb7 */
+class __TwigTemplate_14084eb55da68d6ceb2e4bb3a1074e70d3e3335b73b0373c9f34ec1882bd3af5 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["field_enlace"] ?? null), 1, $this->source), "html", null, true);
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__c74f7d902ba7012a2a03774c8e2e47e3fbaae964cc9f93f530bb52b60626ccb7";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__c74f7d902ba7012a2a03774c8e2e47e3fbaae964cc9f93f530bb52b60626ccb7", "");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 1);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
