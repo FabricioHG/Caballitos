24a2ab022fa183d5b3b37960c99a035fa8fd1895
Rama Fabricio <fabriciofranciscohernandezgome@gmail.com>
2023-01-30 17:27:43 -0600

    Segunda version para subir, la primera no se subio

---
+++ sites/default/files/php/twig/63d851edc79a1___string_template__78021d_kbxRgGR6J3Z-RdltzzUUUrIcP/7nHrYBpNijHvgFLS9umcKmEQZBQSr7GMVaS0U6JIJvc.php
@@ -0,0 +1,91 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__78021de2bbed7db8b6a23995d03a2e9d98aac4ead2de4694a392e6f9d33194d5 */
+class __TwigTemplate_016b5bf1722334203a157111faba49a0dad1c06838e89fd92022df1c2a204f68 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<span class=\"calendar_ico\"><img src=\"/themes/drudg8b3/images/calendar_icon.png\"></span>";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null), 1, $this->source), "html", null, true);
+        echo " ";
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__78021de2bbed7db8b6a23995d03a2e9d98aac4ead2de4694a392e6f9d33194d5";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{# inline_template_start #}<span class=\"calendar_ico\"><img src=\"/themes/drudg8b3/images/calendar_icon.png\"></span>{{ title }} ", "__string_template__78021de2bbed7db8b6a23995d03a2e9d98aac4ead2de4694a392e6f9d33194d5", "");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 1);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
